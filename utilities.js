cfc214e8cc8d1cc3ecede552e2466af9b4ea4c83
Tim <deasti@icloud.com>
2024-02-20 19:21:30 +0000

    added listener

--- utilities.js
+++ utilities.js
@@ -1,11 +1,19 @@
 // alert('Hello, World!');
 
-// function clickOMatic() {
-//     alert('Hello!');
-// }
-
 // let clickOMatic = function() {
 //     alert('Hello');
 // };
 
-let clickOMatic = () => alert('Hello!');
+// let clickOMatic = () => alert('Hello!');
+
+// the following is the way we want to most declare fuctions:
+function clickOMatic() {
+    alert('Hello!');
+}
+
+const click = document.getElementById('click');
+
+click.addEventListener("click", (event) => {
+clickOMatic();
+
+});
